17f336575b1fde7cc0cc7c0727796cfc999860cc
Cat <cmaat082@uottawa.ca>
2015-06-21 16:55:53 -0400

    layout of main page done

---
+++ app/src/main/java/net/wintastic/tipcalc/SuggestTabActivity.java
@@ -0,0 +1,23 @@
+package net.wintastic.tipcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class SuggestTabActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        TextView tv=new TextView(this);
+        tv.setTextSize(25);
+        tv.setGravity(Gravity.CENTER_VERTICAL);
+        tv.setText("This Is Suggest Tab Activity");
+
+        setContentView(tv);
+    }
+
+}
