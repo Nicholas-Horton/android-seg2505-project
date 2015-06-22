ce037115bdd789a9215111f6e9955a135a9b3b22
Nicholas Horton <Nicholas.Oryan.Horton@gmail.com>
2015-06-21 22:27:55 -0400

    Added total amount to pay per person. Added support for custom currency formatting.

--- app/src/main/java/net/wintastic/tipcalc/SummaryActivity.java
+++ app/src/main/java/net/wintastic/tipcalc/SummaryActivity.java
@@ -7,42 +7,57 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 
 
 public class SummaryActivity extends ActionBarActivity {
 
-    private float billAmount  = 0f;
-    private int   numPeople   = 0;
-    private float tipAmount   = 0f;
-    private float tipTotal    = 0f;
-    private float tipToPay    = 0f;
+    private float billAmount      = 0f;
+    private int   numPeople       = 0;
+    private float tipAmount       = 0f;
+    private float tipTotal        = 0f;
+    private float tipToPay        = 0f;
+    private float amountPerPerson = 0f;
 
-    private final NumberFormat formatter = NumberFormat.getCurrencyInstance();
+    private NumberFormat         formatter      = NumberFormat.getCurrencyInstance();
+    private DecimalFormatSymbols currencyFormat = new DecimalFormatSymbols();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
         Intent intent = getIntent();
+
+        //Currency formatting
+        currencyFormat.setCurrencySymbol("$"); //TODO: Placeholder, needs to use settings value
+        currencyFormat.setDecimalSeparator('.');
+        ((DecimalFormat) formatter).setDecimalFormatSymbols(currencyFormat);
+
         billAmount = intent.getFloatExtra("billAmount", 0f);
         numPeople  = intent.getIntExtra("numPeople", 0);
         tipAmount  = intent.getFloatExtra("tipPercent", 0f);
-
         tipTotal   = calculateTipTotal(billAmount, numPeople, tipAmount);
-        if (numPeople == 0){tipToPay = 0f;} else{tipToPay   = tipTotal / numPeople;}
+        float amountToPay = 0f;
+        if (numPeople == 0){tipToPay = 0f; amountToPay = billAmount;}
+        else{tipToPay = tipTotal / numPeople; amountToPay = billAmount / numPeople;}
 
-        TextView txtBillAmount = (TextView) findViewById(R.id.txtBillAmount);
-        TextView txtNumPeople  = (TextView) findViewById(R.id.txtNumPayees);
-        TextView txtTipAmount  = (TextView) findViewById(R.id.txtTipPercent);
-        TextView txtTipToPay   = (TextView) findViewById(R.id.txtTipToPay);
-        TextView txtTipTotal   = (TextView) findViewById(R.id.txtTipTotal);
+        amountPerPerson = amountToPay + tipToPay;
 
-        txtBillAmount.setText("" + formatter.format(billAmount));
-        txtNumPeople.setText( "" + numPeople);
-        txtTipAmount.setText( "" + Math.round(tipAmount * 100) + "%"); // tip amount does not display correctly without rounding
-        txtTipTotal.setText(  "" + formatter.format(tipTotal));
-        txtTipToPay.setText(  "" + formatter.format(tipToPay));
+        TextView txtBillAmount      = (TextView) findViewById(R.id.txtBillAmount);
+        TextView txtNumPeople       = (TextView) findViewById(R.id.txtNumPayees);
+        TextView txtTipAmount       = (TextView) findViewById(R.id.txtTipPercent);
+        TextView txtTipToPay        = (TextView) findViewById(R.id.txtTipToPay);
+        TextView txtTipTotal        = (TextView) findViewById(R.id.txtTipTotal);
+        TextView txtAmountPerPerson = (TextView) findViewById(R.id.txtAmountToPay);
+
+        txtBillAmount.setText(      "" + formatter.format(billAmount));
+        txtNumPeople.setText(       "" + numPeople);
+        txtTipAmount.setText(       "" + Math.round(tipAmount * 100) + "%"); // tip amount does not display correctly without rounding
+        txtTipTotal.setText(        "" + formatter.format(tipTotal));
+        txtTipToPay.setText(        "" + formatter.format(tipToPay));
+        txtAmountPerPerson.setText( "" + formatter.format(amountPerPerson));
 
     }
 
